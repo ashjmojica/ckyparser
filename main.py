f5327256b7d58225561427b690e9fdaf1cccaf9f
Ashley Mojica <ashjmojica@gmail.com>
2023-04-29 08:38:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+allowed_sentences = [
+    "I want to travel",
+    "I like Texas",
+    "I want to swim",
+    "Go to South Padre",
+    "Get a hotel",
+    "On the beach",
+    "I want a view",
+    "Love the sunrise",
+    "A nice vacation",
+    "Book a boat",
+    "Need this trip",
+    "Waves are high",
+    "Prefer the boat",
+    "I enjoy speed",
+    "I love water",
+]
+
+def parse_sentence(sentence):
+    if sentence in allowed_sentences:
+        print("This sentence is allowed.")
+    else:
+        print("This sentence is not allowed.")
+
+# Test the parser with an allowed and a not allowed sentence
+parse_sentence("I want to travel")
